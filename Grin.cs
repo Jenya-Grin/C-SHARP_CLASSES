c3a1e290ad81c34b14a32a5bbf0684a937bddffc
Jenya-Grin <Jenya-Grin@mail.ru>
2016-11-11 14:10:33 +0200

    Upload

---
+++ Grin.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Windows.Forms;
+
+public static class root
+{
+	public static string myvar = "778";
+
+	public static class aaabbb
+	{
+		public static string str = "gggtext";
+
+		public static void showmessage(string text)
+		{
+			MessageBox.Show(text);
+		}
+	}
+}
+
+public static class root2
+{
+	public static string myvar2 = "445"; 
+}
